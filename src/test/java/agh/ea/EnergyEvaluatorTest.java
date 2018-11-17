20d1cfea1152d4d6788cba669665c920e079ada8
joaoperfig <joaoperfig@gmail.com>
2018-11-17 17:39:29 +0100

    change folder structure; add test

---
+++ src/test/java/agh/ea/EnergyEvaluatorTest.java
@@ -0,0 +1,14 @@
+package agh.ea;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class EnergyEvaluatorTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
