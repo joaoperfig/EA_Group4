481fe53323a35123c7a142702a8a4c6add38d9f4
joaoperfig <joaoperfig@gmail.com>
2018-11-17 16:34:36 +0100

    add evaluators

---
+++ src/agh/ea/EnergyEvaluator.java
@@ -0,0 +1,22 @@
+package agh.ea;
+
+public class EnergyEvaluator implements IEvaluator {
+
+	@Override
+	public float evaluate(ISequence sequence) {
+		int total = 0;
+		for (int k=0; k<sequence.getLength(); k++) {
+			total += Math.pow(cks(k, sequence), 2);
+		}
+		return (float)total;
+	}
+	
+	private int cks(int k, ISequence sequence) {
+		int total = 0;
+		for (int i=0; i<sequence.getLength()-k; i++) {
+			total += sequence.at(i)*sequence.at(i+k);
+		}
+		return total;
+	}
+
+}
---
+++ src/agh/ea/MeritEvaluator.java
@@ -0,0 +1,12 @@
+package agh.ea;
+
+public class MeritEvaluator implements IEvaluator {
+
+	private EnergyEvaluator energyEval = new EnergyEvaluator();
+	
+	@Override
+	public float evaluate(ISequence sequence) {
+		return (float) (Math.pow(sequence.getLength(),2)/energyEval.evaluate(sequence));
+	}
+
+}
